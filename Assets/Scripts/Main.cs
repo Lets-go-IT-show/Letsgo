ffe24a358b9b77b2d0fcff7289db47a838be80c3
uyeah0 <s2031@e-mirim.hs.kr>
2022-06-15 00:26:40 +0900

    scenes, scripts 경로 수정 및 화면 크기 조정

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// PlayBtn에서 Scene을 불러오는데 SecneManager를 사용하기 위해서 네임스페이스 추가
+
+
+public class Main : MonoBehaviour
+{
+    // PlayBtn을 버튼의 OnClick이벤트에 연결
+    // PlayBtn에서 SceneManager를 이용해서 Loading을 불러온다
+
+    // 레쭈고 메인 화면
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // 메뉴로 화면 전환
+    public void PlayBtn()
+    {
+        //SceneManager.LoadScene("불러올 씬이름 or 번호");
+        SceneManager.LoadScene("Menu");
+    }
+
+    // 이름 입력창
+    public void GameName()
+    {
+        SceneManager.LoadScene("Name");
+    }
+
+    // 메뉴에서 시작 -> 게임 시작
+    public void GameStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // 메뉴에서 시작 -> 순위
+    public void GameRank()
+    {
+        SceneManager.LoadScene("Rank");
+    }
+
+    // 메뉴에서 방법 -> 게임 방법
+    public void GameRole()
+    {
+        SceneManager.LoadScene("Role");
+    }
+
+
+    public void GameRole_2()
+    {
+        SceneManager.LoadScene("Role_2");
+    }
+
+
+}
---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+// Slider와 Sceneager를 사용하기 위해서 UI와 SceneManagement 네임스페이스를 추가
+
+public class SceneLoad : MonoBehaviour
+{
+
+    // 비동기 로드(LoadScenceAsyne)를 만들기 위해서 코루틴을 사용합니다
+    // 비동기 로드는 Scene을 불러올 때 멈추지 않고 다른 작업을 할 수 있다
+    // LodeScene()로 Scene을 불러오면 완료될 때까지 다른 작업을 수행하지 않습니다
+    public Slider progressbar;
+    public Text loadtext;
+
+    // Start함수 안에서 LoadScene를 실행합니다
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(3); // Play를 비동기식으로 불러온다
+        // 이때 LoadSceneAsync가 AsycnOperation을 반환하니 담아줍니다
+        operation.allowSceneActivation = false;
+
+        // 반복문 : Slider의 value를 매 프레임증가
+        while (!operation.isDone)
+        {
+            yield return null;
+            if (progressbar.value < 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
+            }
+            else if (operation.progress >= 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+            }
+
+            if (progressbar.value >= 1f)
+            {
+                loadtext.text = "Press SpaceBar";
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && progressbar.value >= 1f && operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
+    }
+
+
+
+    /* 
+    AsyncOperation 속성 
+     : isDone은 작업의 완료 유무를 boolean 형으로 반환합니다
+     : progress는 진행정도를 float형 0,1을 반환합니다 (0 : 진행중, 1 : 진행완료)
+     : allowSceneActivation은 true면 로딩이 완료되면 바로 신을 넘기고 false이면 progress가 0.9f에서 정지
+       이때 다시 true를 해야 불러온 Scene으로 넘어갈 수 있다
+    
+    operation.isDone;
+    operation.progress;
+    operation.allowSceneActivation;
+    */
+
+}
