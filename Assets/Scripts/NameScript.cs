053b4728aeede85b3d023c47ecd1a7aecf858b31
uyeah0 <s2031@e-mirim.hs.kr>
2022-06-15 00:26:40 +0900

    scenes, scripts 경로 수정 및 화면 크기 조정

---
+++ Assets/Scripts/NameScript.cs
@@ -0,0 +1,16 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NameScript : MonoBehaviour
+{
+    public InputField inputName;
+    public void Save()
+    {
+        PlayerParefs.SetString("Name", inputName.test);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+*/
