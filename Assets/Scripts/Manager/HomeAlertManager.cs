35c38e56e84c1699779344517b9e3aa4560b39f2
Yeyeong Yu <s2031@e-mirim.hs.kr>
2022-06-21 14:15:05 +0900

    feat(alert) : 다시, 홈 버튼 alert 구현 (#5)

---
+++ Assets/Scripts/Manager/HomeAlertManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeAlertManager : MonoBehaviour
+{
+    [SerializeField] GameObject goUI = null;
+
+    public void ShowAlter()
+    {
+        AudioManager.instance.StopBGM();
+        goUI.SetActive(true);
+
+        if (Time.timeScale == 0) //멈춰있으면
+        {
+            Time.timeScale = 1f; //시작
+        }
+        else //움직이면
+        {
+            Time.timeScale = 0; //멈추기
+        }
+
+    }
+
+    public void noteStart()
+    {
+        AudioManager.instance.StopBGM();
+        if (Time.timeScale == 0) //멈춰있으면
+        {
+            Time.timeScale = 1f; //시작
+            goUI.SetActive(false); // 알림창 가리기
+        }
+        else //움직이면
+        {
+            Time.timeScale = 0; //멈추기
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Manager/ReturnAlertManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnAlertManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject goUI = null;
+
+    public void ShowAlter()
+    {
+        AudioManager.instance.StopBGM();
+        goUI.SetActive(true);
+
+        if (Time.timeScale == 0) //멈춰있으면
+        {
+            Time.timeScale = 1f; //시작
+        }
+        else //움직이면
+        {
+            Time.timeScale = 0; //멈추기
+        }
+
+    }
+
+    public void noteStart()
+    {
+
+        if (Time.timeScale == 0) //멈춰있으면
+        {
+            Time.timeScale = 1f; //시작
+            goUI.SetActive(false); // 알림창 가리기
+            AudioManager.instance.StopBGM();
+        }
+        else //움직이면
+        {
+            Time.timeScale = 0; //멈추기
+        }
+    }
+
+
+}
